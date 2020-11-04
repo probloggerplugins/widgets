fedd2dbdb847e21e95dbda981b0829c3d58b5b7e
probloggerplugins <73790652+probloggerplugins@users.noreply.github.com>
2020-11-04 03:29:08 +0100

    Update tag-cloud.js

--- tag-cloud.js
+++ tag-cloud.js
@@ -41,7 +41,7 @@ pbpTagCloud = typeof pbpTagCloud == 'undefined' ? 0 : pbpTagCloud+1;
 		sciezka.setAttribute('d', 'M190.5 66.9l22.2-22.2c9.4-9.4 24.6-9.4 33.9 0L441 239c9.4 9.4 9.4 24.6 0 33.9L246.6 467.3c-9.4 9.4-24.6 9.4-33.9 0l-22.2-22.2c-9.5-9.5-9.3-25 .4-34.3L311.4 296H24c-13.3 0-24-10.7-24-24v-32c0-13.3 10.7-24 24-24h287.4L190.9 101.2c-9.8-9.3-10-24.8-.4-34.3z');
 		svg.appendChild(sciezka);
 		szukacz.appendChild(svg);
-		let szukinfo = document.createElement('span');
+		var szukinfo = document.createElement('span');
 		szukinfo.setAttribute('class', 'searchInfo');
 		szukinfo.textContent = 'No selected labels to search';
 		szukacz.appendChild(szukinfo);
@@ -127,9 +127,16 @@ pbpTagCloud = typeof pbpTagCloud == 'undefined' ? 0 : pbpTagCloud+1;
 							if (czy) licznik++;
 						}
 						ileZnal.textContent = licznik;
+						if (licznik === 0) {
+							szukacz.classList.add('unactive');
+							szukinfo.textContent = 'No posts tagged with all checked labels';
+						} else {
+							szukinfo.textContent = 'Show ' + licznik + ' posts with all selected labels';
+						}
 					} else {
 						szukacz.classList.add('unactive');
 						ileZnal.textContent = '0';
+						szukinfo.textContent = 'No selected labels to search';
 					}
 				}
 				div.appendChild(czek);
