932a41d7adb05602ecfa7c85ecb7e25b9744ea57
probloggerplugins <73790652+probloggerplugins@users.noreply.github.com>
2020-11-08 05:13:35 +0100

    Update tag-cloud.js

--- tag-cloud.js
+++ tag-cloud.js
@@ -1,53 +1,54 @@
 pbpTagCloud = typeof pbpTagCloud == 'undefined' ? 0 : pbpTagCloud+1;
 
-(function(nr, skryptSRC) {
+(function(nr, doc, g, q, skryptSRC) {
 	let wszysTagi = [];
 	let posTagi = [];
-	let elem = document.createElement('div');
+	let elem = doc.createElement('div');
 	let znaki = 'qwertyuiopasdfghjklzxcvbnmQWERTYUIOPASDFGHJKLZXCVBNM1234567890_-';
 	let d = 'pbpTC-';
 	while (d.length < 16) {
 		d += znaki[Math.floor(Math.random() * znaki.length)];
 	}
+	
 	elem.id = d;
-	let skrypt = document.querySelectorAll(skryptSRC)[nr];
+	let skrypt = doc[q](skryptSRC)[nr];
 	skrypt.parentNode.insertBefore(elem, skrypt);
 	
-	let textSize = Number(skrypt.getAttribute('textSize'));
+	let textSize = Number(skrypt[g]('textSize'));
 	if (textSize < 1 || isNaN(textSize)) textSize = 16;
 	
-	let display = skrypt.getAttribute('display');
+	let display = skrypt[g]('display');
 	if (display !== 'list' && display !== 'cloud') display = 'inline';
 	
-	let searchText = skrypt.getAttribute('searchText') ? skrypt.getAttribute('searchText') : 'Search';
+	let searchText = skrypt[g]('searchText') ? skrypt[g]('searchText') : 'Search';
 	
-	let sortText = skrypt.getAttribute('sortText') ? skrypt.getAttribute('sortText') : 'Sort:';
+	let sortText = skrypt[g]('sortText') ? skrypt[g]('sortText') : 'Sort:';
 	
-	let sortBy = skrypt.getAttribute('sortBy') === 'popularity' ? 'popularity' : 'alphabetically';
+	let sortBy = skrypt[g]('sortBy') === 'popularity' ? 'popularity' : 'alphabetically';
 	
-	let alphabeticallyText = skrypt.getAttribute('alphabeticallyText') ? skrypt.getAttribute('alphabeticallyText') : 'Alphabetically';
+	let alphabeticallyText = skrypt[g]('alphabeticallyText') ? skrypt[g]('alphabeticallyText') : 'Alphabetically';
 	
-	let byPopularityText = skrypt.getAttribute('byPopularityText') ? skrypt.getAttribute('byPopularityText') : 'By popularity';
+	let byPopularityText = skrypt[g]('byPopularityText') ? skrypt[g]('byPopularityText') : 'By popularity';
 	
-	let combining = skrypt.getAttribute('combining') === 'false' ? false : true;
+	let combining = skrypt[g]('combining') === 'false' ? false : true;
 	
-	let sorter = skrypt.getAttribute('sorter') === 'false' ? false : true;
+	let sorter = skrypt[g]('sorter') === 'false' ? false : true;
 	
-	let showCounter = skrypt.getAttribute('showCounter') === 'false' ? false : true;
+	let showCounter = skrypt[g]('showCounter') === 'false' ? false : true;
 	
-	let borderWidth = skrypt.getAttribute('borderWidth') ? Number(skrypt.getAttribute('borderWidth')) : 1;
+	let borderWidth = skrypt[g]('borderWidth') ? Number(skrypt[g]('borderWidth')) : 1;
 	if (borderWidth < 0 || isNaN(borderWidth)) borderWidth = 1;
 	
-	let textColor = skrypt.getAttribute('textColor') ? skrypt.getAttribute('textColor') : 'black';
+	let textColor = skrypt[g]('textColor') ? skrypt[g]('textColor') : 'black';
 	
-	let borderColor = skrypt.getAttribute('borderColor') ? skrypt.getAttribute('borderColor') : textColor;
+	let borderColor = skrypt[g]('borderColor') ? skrypt[g]('borderColor') : textColor;
 	
-	let borderRadius = skrypt.getAttribute('borderRadius') ? Number(skrypt.getAttribute('borderRadius')) : 5;
+	let borderRadius = skrypt[g]('borderRadius') ? Number(skrypt[g]('borderRadius')) : 5;
 	if (borderRadius < 0 || isNaN(borderRadius)) borderRadius = 5;
 	
-	let background = skrypt.getAttribute('background') ? skrypt.getAttribute('background') : 'transparent';
+	let background = skrypt[g]('background') ? skrypt[g]('background') : 'transparent';
 	
-	let tagIcon = skrypt.getAttribute('tagIcon');
+	let tagIcon = skrypt[g]('tagIcon');
 	if (tagIcon === 'false') {
 		tagIcon = false;
 	} else if (tagIcon === 'true') {
@@ -56,7 +57,7 @@ pbpTagCloud = typeof pbpTagCloud == 'undefined' ? 0 : pbpTagCloud+1;
 		tagIcon = combining ? false : true;
 	}
 	
-	let textAlign = skrypt.getAttribute('textAlign');
+	let textAlign = skrypt[g]('textAlign');
 	if (textAlign !== 'right' && textAlign !== 'center') textAlign = 'left';
 	
 	let ilMin = 0;
@@ -65,10 +66,10 @@ pbpTagCloud = typeof pbpTagCloud == 'undefined' ? 0 : pbpTagCloud+1;
 	elem.style.textAlign = textAlign;
 	
 	if (sorter) {
-		let dv = document.createElement('div');
+		let dv = doc.createElement('div');
 		dv.style.marginBottom = '10px';
 		dv.innerHTML = '<span style="font-size:' + textSize + 'px;margin-right:6px;">' + sortText + '</span>';
-		let sel = document.createElement('select');
+		let sel = doc.createElement('select');
 		sel.innerHTML = '<option value="alphabetically"' + (sortBy !== 'popularity' ? ' selected' : '') + '>' + alphabeticallyText + '</option><option value="popularity"' + (sortBy === 'popularity' ? ' selected' : '') + '>' +byPopularityText + '</option>';
 		sel.style.padding = '2px 5px';
 		sel.style.border = '1px solid #333333';
@@ -86,44 +87,44 @@ pbpTagCloud = typeof pbpTagCloud == 'undefined' ? 0 : pbpTagCloud+1;
 		elem.appendChild(dv);
 	}
 	
-	let glowny = document.createElement('div');
+	let glowny = doc.createElement('div');
 	elem.appendChild(glowny);
 	
 	if (combining) {
-		let dv = document.createElement('div');
+		let dv = doc.createElement('div');
 		dv.style.marginTop = '10px';
-		var szukacz = document.createElement('span');
+		var szukacz = doc.createElement('span');
 		szukacz.setAttribute('class', 'pbpTC_searchButton unactive');
 		szukacz.innerHTML = searchText + ' (';
-		var ileZnal = document.createElement('span');
+		var ileZnal = doc.createElement('span');
 		ileZnal.textContent = '0';
 		szukacz.appendChild(ileZnal);
-		szukacz.appendChild(document.createTextNode(')'));
-		let svg = document.createElementNS('http://www.w3.org/2000/svg', 'svg');
+		szukacz.appendChild(doc.createTextNode(')'));
+		let svg = doc.createElementNS('http://www.w3.org/2000/svg', 'svg');
 		svg.setAttribute('viewBox', '0 0 448 512');
-		let sciezka = document.createElementNS('http://www.w3.org/2000/svg', 'path');
+		let sciezka = doc.createElementNS('http://www.w3.org/2000/svg', 'path');
 		sciezka.setAttribute('d', 'M190.5 66.9l22.2-22.2c9.4-9.4 24.6-9.4 33.9 0L441 239c9.4 9.4 9.4 24.6 0 33.9L246.6 467.3c-9.4 9.4-24.6 9.4-33.9 0l-22.2-22.2c-9.5-9.5-9.3-25 .4-34.3L311.4 296H24c-13.3 0-24-10.7-24-24v-32c0-13.3 10.7-24 24-24h287.4L190.9 101.2c-9.8-9.3-10-24.8-.4-34.3z');
 		svg.appendChild(sciezka);
 		szukacz.appendChild(svg);
-		var szukinfo = document.createElement('span');
+		var szukinfo = doc.createElement('span');
 		szukinfo.setAttribute('class', 'searchInfo');
 		szukinfo.textContent = 'No selected labels to search';
 		szukacz.appendChild(szukinfo);
 		szukacz.onclick = function() {
 			if (!this.classList.contains('unactive')) {
 				let url = '/search/label/';
-				glowny.querySelectorAll('input[type="checkbox"]:checked').forEach((lab, i) => {
+				glowny[q]('input[type="checkbox"]:checked').forEach((lab, i) => {
 					url += (i > 0 ? '+' : '') + encodeURIComponent(lab.value);
 				});
 				location.href = url;
 			}
 		}
 		dv.appendChild(szukacz);
-		let odznacz = document.createElement('span');
+		let odznacz = doc.createElement('span');
 		odznacz.innerHTML = '<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512"><path fill="currentColor" d="M400 32H48C21.5 32 0 53.5 0 80v352c0 26.5 21.5 48 48 48h352c26.5 0 48-21.5 48-48V80c0-26.5-21.5-48-48-48zm16 400c0 8.8-7.2 16-16 16H48c-8.8 0-16-7.2-16-16V80c0-8.8 7.2-16 16-16h352c8.8 0 16 7.2 16 16v352zm-97.2-245.3L249.5 256l69.3 69.3c4.7 4.7 4.7 12.3 0 17l-8.5 8.5c-4.7 4.7-12.3 4.7-17 0L224 281.5l-69.3 69.3c-4.7 4.7-12.3 4.7-17 0l-8.5-8.5c-4.7-4.7-4.7-12.3 0-17l69.3-69.3-69.3-69.3c-4.7-4.7-4.7-12.3 0-17l8.5-8.5c4.7-4.7 12.3-4.7 17 0l69.3 69.3 69.3-69.3c4.7-4.7 12.3-4.7 17 0l8.5 8.5c4.6 4.7 4.6 12.3 0 17z"></path></svg>Uncheck all';
 		odznacz.setAttribute('class', 'pbpTC_uncheck');
 		odznacz.onclick = function() {
-			glowny.querySelectorAll('input[type="checkbox"]:checked').forEach(i => {
+			glowny[q]('input[type="checkbox"]:checked').forEach(i => {
 				i.checked = false;
 				i.parentNode.classList.remove('active');
 			});
@@ -135,7 +136,7 @@ pbpTagCloud = typeof pbpTagCloud == 'undefined' ? 0 : pbpTagCloud+1;
 		elem.appendChild(dv);
 	}
 	
-	let styl = document.createElement('style');
+	let styl = doc.createElement('style');
 	styl.innerHTML = `#${d} div.pbpLabel{margin:3px 8px;display:${display !== 'list' ? 'inline-flex' : 'flex'};align-items:center;${combining ? 'border:' + borderWidth + 'px solid ' + borderColor + ';border-radius:'+borderRadius+'px;background:' + background + ';padding:2px;' : ''}}
 
 #${d} div.pbpLabel a{display:inline-flex;align-items:center;text-decoration:none;padding:2px 5px 2px ${combining ? '1' : '5'}px;font-size:${textSize}px;color:${textColor};${!combining ? 'border:' + borderWidth + 'px solid ' + borderColor + ';border-radius:'+borderRadius+'px;background:' + background : ''}}
@@ -170,14 +171,14 @@ ${combining ? '#' + d + ' div.pbpLabel:hover a{text-decoration:underline;}' : '#
 
 #${d} .pbpTC_uncheck svg {height:${textSize-1}px;margin-right:3px;}`
 	
-	document.head.appendChild(styl);
+	doc.head.appendChild(styl);
 
 	function lapWszystko(f) {
 		let wpisy = f.responseXML.getElementsByTagName('entry');
 		for (let q=0; q<wpisy.length; q++) {
 			let kategorie = [];
-			wpisy[q].querySelectorAll('category').forEach(e => {
-				let kat = e.getAttribute('term');
+			wpisy[q][q]('category').forEach(e => {
+				let kat = e[g]('term');
 				kategorie.push(kat);
 				let ten = wszysTagi.filter(a => a.k === kat);
 				if (ten.length > 0) {
@@ -216,10 +217,10 @@ ${combining ? '#' + d + ' div.pbpLabel:hover a{text-decoration:underline;}' : '#
 		}
 		wszysTagi.sort((a, b) => sort === 'popularity' ? b.i - a.i : a.k.localeCompare(b.k));
 		wszysTagi.forEach(t => {
-			let div = document.createElement('div');
+			let div = doc.createElement('div');
 			div.setAttribute('class', 'pbpLabel')
 			if (combining) {
-				let czek = document.createElement('input');
+				let czek = doc.createElement('input');
 				czek.type = 'checkbox';
 				czek.value = t.k;
 				czek.oninput = function() {
@@ -228,7 +229,7 @@ ${combining ? '#' + d + ' div.pbpLabel:hover a{text-decoration:underline;}' : '#
 					} else {
 						div.classList.remove('active');
 					}
-					let zazny = glowny.querySelectorAll('input[type="checkbox"]:checked');
+					let zazny = glowny[q]('input[type="checkbox"]:checked');
 					if (zazny.length) {
 						let licznik = 0;
 						for (let i=0; i<posTagi.length; i++) {
@@ -256,7 +257,7 @@ ${combining ? '#' + d + ' div.pbpLabel:hover a{text-decoration:underline;}' : '#
 				}
 				div.appendChild(czek);
 			}
-			let a = document.createElement('a');
+			let a = doc.createElement('a');
 			a.href = '/search/label/' + t.k;
 			a.innerHTML = (tagIcon ? '<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"><path d="M0 252.118V48C0 21.49 21.49 0 48 0h204.118a48 48 0 0 1 33.941 14.059l211.882 211.882c18.745 18.745 18.745 49.137 0 67.882L293.823 497.941c-18.745 18.745-49.137 18.745-67.882 0L14.059 286.059A48 48 0 0 1 0 252.118zM112 64c-26.51 0-48 21.49-48 48s21.49 48 48 48 48-21.49 48-48-21.49-48-48-48z"></path></svg>' : '') + t.k + (showCounter ? ' (' + t.i + ')' : '');
 			if (display === 'cloud') {
@@ -278,4 +279,4 @@ ${combining ? '#' + d + ' div.pbpLabel:hover a{text-decoration:underline;}' : '#
 
 	zapyt(1, 150);
 
-})(pbpTagCloud, 'script[src="https://probloggerplugins.github.io/widgets/tag-cloud.js"]');
+})(pbpTagCloud, document, 'getAttribute', 'querySelectorAll', 'script[src="https://probloggerplugins.github.io/widgets/tag-cloud.js"]');
