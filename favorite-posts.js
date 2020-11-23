e3e3cd7fb10ff47823261e5136435ffeb3a2f1c7
probloggerplugins <73790652+probloggerplugins@users.noreply.github.com>
2020-11-23 23:59:48 +0100

    Update favorite-posts.js

--- favorite-posts.js
+++ favorite-posts.js
@@ -119,7 +119,7 @@ var pbpFavPostsCnt = typeof pbpFavPostsCnt == 'undefined' ? 0 : pbpFavPostsCnt +
 		okno.appendChild(cofdyw);
 		let wstecz = document.createElement('span');
 		wstecz.setAttribute('class', 'pbpArrowBack');
-		wstecz.innerHTML = '<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512"><path d="M257.5 445.1l-22.2 22.2c-9.4 9.4-24.6 9.4-33.9 0L7 273c-9.4-9.4-9.4-24.6 0-33.9L201.4 44.7c9.4-9.4 24.6-9.4 33.9 0l22.2 22.2c9.5 9.5 9.3 25-.4 34.3L136.6 216H424c13.3 0 24 10.7 24 24v32c0 13.3-10.7 24-24 24H136.6l120.5 114.8c9.8 9.3 10 24.8.4 34.3z"></path></svg>';
+		wstecz.innerHTML = '<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512"><path fill="currentColor" d="M257.5 445.1l-22.2 22.2c-9.4 9.4-24.6 9.4-33.9 0L7 273c-9.4-9.4-9.4-24.6 0-33.9L201.4 44.7c9.4-9.4 24.6-9.4 33.9 0l22.2 22.2c9.5 9.5 9.3 25-.4 34.3L136.6 216H424c13.3 0 24 10.7 24 24v32c0 13.3-10.7 24-24 24H136.6l120.5 114.8c9.8 9.3 10 24.8.4 34.3z"></path></svg>';
 		wstecz.onclick = function() {
 			document.body.removeChild(okno);
 		}
@@ -213,9 +213,9 @@ var pbpFavPostsCnt = typeof pbpFavPostsCnt == 'undefined' ? 0 : pbpFavPostsCnt +
 .pbpArrowBack:hover svg{height:24px;}
 .pbpArrowBack:active{padding:1px 7px 1px 1px;}
 
-.FavPostsPageTitle{font-size:22px;background:#323232;color:white;padding-bottom:10px;font-weight:bold;}
+.FavPostsPageTitle{font-size:22px;background:#323232;color:white;padding-bottom:8px;font-weight:bold;margin-bottom:16px;text-align:center;}
 
-.pbpFavPostBox{padding:7px;border:1px solid black;border-radius:10px;}
+.pbpFavPostBox{padding:7px;border:1px solid black;border-radius:10px;margin-bottom:12px;}
 
 .favorite-post-title{margin-bottom:10px;}
 .favorite-post-title a{font-size:18px;color:blue;text-decoration:none;font-family:"Palatino Linotype","Book Antiqua",Palatino,serif;}`;
