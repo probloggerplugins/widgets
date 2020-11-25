84f96701a74ce7c178d77d2e756ff3f6b34b96b6
probloggerplugins <73790652+probloggerplugins@users.noreply.github.com>
2020-11-25 20:16:48 +0100

    Update favorite-posts.js

--- favorite-posts.js
+++ favorite-posts.js
@@ -30,7 +30,7 @@ var pbpFavPostsCnt = typeof pbpFavPostsCnt == 'undefined' ? 0 : pbpFavPostsCnt +
 	
 	function pierdolnij(gdzie, url) {
 		let dyw = document.createElement('div');
-		dyw.style.position = 'relative';
+		dyw.style.textAlign = position;
 		let ul = document.createElement('span');
 		ul.setAttribute('class', 'pbpFavourite');
 		let inf = document.createElement('span');
@@ -125,6 +125,9 @@ var pbpFavPostsCnt = typeof pbpFavPostsCnt == 'undefined' ? 0 : pbpFavPostsCnt +
 	
 	let iconType = skrypt.getAttribute('iconType');
 	if (iconType !== 'heart' && iconType !== 'star') iconType = 'heart';
+	
+	let position = skrypt.getAttribute('position');
+	if (position !== 'left' && position !== 'center') position = 'right';
 	
 	
 	function wyswietl(postId, dyw) {
@@ -233,7 +236,7 @@ var pbpFavPostsCnt = typeof pbpFavPostsCnt == 'undefined' ? 0 : pbpFavPostsCnt +
 	
 	let styl = document.createElement('style');
 	styl.innerHTML = `
-.pbpFavourite{display:inline-block;position:absolute;right:5px;top:5px;width:${iconSize+2}px;height:${iconSize+2}px;background-repeat:no-repeat;background-size:${iconSize}px ${iconSize}px;background-position:1px 1px;cursor:pointer;z-index:99999999;}
+.pbpFavourite{display:inline-block;width:${iconSize+2}px;height:${iconSize+2}px;background-repeat:no-repeat;background-size:${iconSize}px ${iconSize}px;background-position:1px 1px;cursor:pointer;z-index:99999999;}
 
 .pbpFavourite[active="false"] {background-image:url('data:image/svg+xml,<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"><path fill="black" d="M458.4 64.3C400.6 15.7 311.3 23 256 79.3 200.7 23 111.4 15.6 53.6 64.3-21.6 127.6-10.6 230.8 43 285.5l175.4 178.7c10 10.2 23.4 15.9 37.6 15.9 14.3 0 27.6-5.6 37.6-15.8L469 285.6c53.5-54.7 64.7-157.9-10.6-221.3zm-23.6 187.5L259.4 430.5c-2.4 2.4-4.4 2.4-6.8 0L77.2 251.8c-36.5-37.2-43.9-107.6 7.3-150.7 38.9-32.7 98.9-27.8 136.5 10.5l35 35.7 35-35.7c37.8-38.5 97.8-43.2 136.5-10.6 51.1 43.1 43.5 113.9 7.3 150.8z"></path></svg>')}
 
